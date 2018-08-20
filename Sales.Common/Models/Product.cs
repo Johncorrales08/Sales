7682a54166abb65a818ded185b5b79b876a2f218
Johncorrales08 <johncorrales8@hotmail.com>
2018-08-20 18:57:45 -0500

    Video 4

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+
+        [Required]
+
+        public int ProductId { get; set; } 
+        
+        public string Descripcion { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishOn { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
